4d94bdab80be77b48e98b8d1c6d4e806998f7975
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-20 10:41:41 +0200

    1 file modified

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { beersData } from '../data/beer-list';
+import { Beer } from '../model/model';
 
 @Injectable({providedIn: 'root'})
 export class BeerService {
 
+  private beers: Beer[] = beersData;
+
   constructor() {} 
   getBeerList(){
-    return beersData;
+    return this.beers;
   }
+  deleteBeer(item : Beer){
+    this.beers = this.beers.filter(beer => beer.id != item.id)
+  }
+
+
+
   
 }
