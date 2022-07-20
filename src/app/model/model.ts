a47c43b08e18b7a0e01d424b5d1ebbff196ab548
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-20 11:44:35 +0200

    1 file modified

--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -36,7 +36,7 @@ export interface Beer {
   lastModifiedDate: Date;
 }
 
-export interface BeerForma{
+export interface BeerForm{
   beerName: FormControl<string | undefined | null>
   beerStyle: FormControl<BeerStyle |undefined |null>
   upc: FormControl<string| undefined |null>
