c8446417537f9f46c56ec75340ebd899949dd6c0
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-20 12:09:54 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './components/beer-list/beer-list.component';
@@ -9,7 +9,7 @@ import { BeerFormComponent } from './components/beer-form/beer-form.component';
 import { BeerComponent } from './components/beer/beer.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule,ReactiveFormsModule],
   declarations: [AppComponent, BeerListComponent, BeerFormComponent, BeerComponent],
   bootstrap: [AppComponent],
 })
