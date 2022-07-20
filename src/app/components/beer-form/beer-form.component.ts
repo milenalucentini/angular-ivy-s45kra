7594dde6a24a30e0d4a40ffbb5bd70cc7dc8c62c
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-20 11:46:57 +0200

    1 file modified

--- src/app/components/beer-form/beer-form.component.ts
+++ src/app/components/beer-form/beer-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { allBeerTypes } from '../../model/model';
 
 @Component({
   selector: 'app-beer-form',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerFormComponent implements OnInit {
 
+  beerTypes = allBeerTypes;
   constructor() { }
 
   ngOnInit() {
