a9f7a6158209f0f18341bde3f95da9d44d93c2a5
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-21 16:48:59 +0200

    3 files modified

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -2,11 +2,15 @@ import { createReducer, on } from "@ngrx/store";
 
 import { beersData } from "../data/beer-list";
 import { BeerState } from "../model/state";
+import { addBeerAction } from "./actions";
 
 
 const initialBeerState: BeerState = {list : beersData};
 export const beerReducers = createReducer(initialBeerState,
-  on(delectAction,(state: BeerState, arg: {id : number}) => ({...state,list: state.list.filter((beer) =>beer.id != arg.id) }))
+  on(Action.delecBeertAction,(state: BeerState, arg: {id : number}) => ({...state,list: state.list.filter((beer) =>beer.id != arg.id), }))
   );
 
-
+  export const addreduce = createReducer(initialBeerState,
+    on(Action.addBeerAction,(state: BeerState, arg: {beer: Beer}) => ({...state,list: pushBeer(state,  arg.beer), }))
+    );
+  
