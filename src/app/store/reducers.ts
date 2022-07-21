86feeb6eb7115c5ae6557bc611c7a5f65f33be11
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-21 16:06:41 +0200

    1 file modified

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -6,7 +6,7 @@ import { BeerState } from "../model/state";
 
 const initialBeerState: BeerState = {list : beersData};
 export const beerReducers = createReducer(initialBeerState,
-  on(delectAction,(state: BeerState, arg: {id : number}) => ({...state,list: state.list.filter((beer) =>beer.id != args.id) }))
+  on(delectAction,(state: BeerState, arg: {id : number}) => ({...state,list: state.list.filter((beer) =>beer.id != arg.id) }))
   );
 
 
