4fd973e31038607c8e788a109379df6a613053c8
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-21 16:54:06 +0200

    1 file modified

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -2,6 +2,6 @@ import { createAction, props } from "@ngrx/store";
 import { Beer } from "../model/model";
 export const deleteBeerAction = createAction('[beer] Decrease',props<{id : number}>());
 export const addBeerAction = createAction('[beer] Icrease',props<{ beer:Beer}>());
-);
+
 
 
