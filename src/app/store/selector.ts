b5cddac37122554204b2f18024949319f0c0b100
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-21 15:01:56 +0200

    1 file modified

--- src/app/store/selector.ts
+++ src/app/store/selector.ts
@@ -1,11 +1,7 @@
-
-import { state } from "@angular/animations";
-import { createSelector } from "@ngrx/store";
-import { AppState, BeerState } from "../model/state";
-const beerStateFunction = (sate: AppState)=> state.beerState;
+import { createSelector } from '@ngrx/store';
+import { AppState, BeerState } from '../model/state';
+const beerStateFunction = (state: AppState) => state.beerState;
 export const beerSelectors = createSelector(
-beerStateFunction,
-(beerState:BeerState)=> beerState.list
-
-
+  beerStateFunction,
+  (beerState: BeerState) => beerState.list
 );
