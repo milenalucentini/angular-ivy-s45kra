1ccc1b6bafeaa7c536246aa8d42ab7eb201f40cc
milenalucentini <milena.lucentini@finconsgroup.com>
2022-07-21 14:34:00 +0200

    1 file modified

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,1 +1,6 @@
-export const beerReducers: any = null;
+import { createReducer } from "@ngrx/store";
+import { beersData } from "../data/beer-list";
+import { BeerState } from "../model/state";
+
+const initialBeerState: BeerState = {list : beersData};
+export const beerReducers: any = createReducer(initialBeerState);
